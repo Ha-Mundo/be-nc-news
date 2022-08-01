706cc1e4508ff1ce0e5e2704d0c117f2cf2191b3
Ha-Mundo <trauzzu@gmail.com>
2022-08-01 20:31:04 +0100

    errors handling folder added

---
+++ errors/index.js
@@ -0,0 +1,15 @@
+exports.psqlErrors = (err, req, res, next) => {
+  if (err.code === "22P02" || err.code === "23502") {
+    res.status(400).send({ msg: "bad request" });
+  } else next(err);
+};
+
+exports.customErrors = (err, req, res, next) => {
+  if (err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else next(err);
+};
+
+exports.handle500s = (err, req, res, next) => {
+  res.status(500).send({ msg: "Server error" });
+};
