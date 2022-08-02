9690c7252ee57023c67fa641ce1aa5f5801eba94
Ha-Mundo <trauzzu@gmail.com>
2022-08-02 14:37:11 +0100

    "GET/api/articles/:article_id" tests added

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -27,3 +27,42 @@ describe("GET /api/topics", () => {
       });
   });
 });
+
+describe("GET/api/articles/:article_id", () => {
+  test("Status:200 and responds with an article object by requested id ", () => {
+    const articleId = 1;
+    return request(app)
+      .get(`/api/articles/${articleId}`)
+      .expect(200)
+      .then(response => {
+        expect(typeof response.body.article).toBe("object");
+        expect(response.body.article).toEqual(
+          expect.objectContaining({
+            article_id: articleId,
+            title: "Living in the shadow of a great man",
+            body: "I find this existence challenging",
+            votes: 100,
+            topic: "mitch",
+            author: "butter_bridge",
+            created_at: "2020-07-09T20:11:00.000Z",
+          })
+        );
+      });
+  });
+  test('Status:400 returns "bad request"', () => {
+    return request(app)
+      .get("/api/articles/abc")
+      .expect(400)
+      .then(response => {
+        expect(response.body.msg).toBe("bad request");
+      });
+  });
+  test('Status:404 and returns "Article not found"', () => {
+    return request(app)
+      .get("/api/articles/99999")
+      .expect(404)
+      .then(response => {
+        expect(response.body.msg).toEqual("Article not found");
+      });
+  });
+});
