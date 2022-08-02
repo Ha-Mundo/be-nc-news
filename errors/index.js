3dd243957ce4064ebb8d13dc0bfc96887e1725ff
Ha-Mundo <trauzzu@gmail.com>
2022-08-02 14:34:29 +0100

    404 errror handling added

--- errors/index.js
+++ errors/index.js
@@ -7,5 +7,9 @@ exports.customErrors = (err, req, res, next) => {
 exports.psqlErrors = (err, req, res, next) => {
   if (err.code === "22P02" || err.code === "23502") {
     res.status(400).send({ msg: "bad request" });
-  }
+  } else next(err);
+};
+
+exports.handle404 = (req, res) => {
+  res.status(404).send({ msg: "Not Found" });
 };
