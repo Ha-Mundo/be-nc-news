5ef3e9f292b072fc64490c841a85815e909e6597
Ha-Mundo <trauzzu@gmail.com>
2022-08-03 12:20:04 +0100

    GET /api/users router + imports added

--- app.js
+++ app.js
@@ -2,16 +2,25 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const { psqlErrors, customErrors, handle404 } = require("./errors/index.js");
+const {
+  psqlErrors,
+  customErrors,
+  handle404,
+  handleFiveHundreds,
+} = require("./errors/index.js");
+
 const { getTopics } = require("./controllers/topics.controllers.js");
 const {
   getArticleById,
   getVotes,
 } = require("./controllers/articles.controllers.js");
+const { getUsers } = require("./controllers/users.controllers.js");
 
 app.get("/api/topics", getTopics);
 app.get("/api/articles/:article_id", getArticleById);
 app.patch("/api/articles/:article_id", getVotes);
+
+app.get("/api/users", getUsers);
 
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "Path not found" });
@@ -20,5 +29,6 @@ app.all("*", (req, res) => {
 app.use(psqlErrors);
 app.use(customErrors);
 app.use(handle404);
+app.use(handleFiveHundreds);
 
 module.exports = app;
