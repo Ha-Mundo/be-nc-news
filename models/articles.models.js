5f129fa2f552c822772571f2c4fd157f0be40d1b
Ha-Mundo <trauzzu@gmail.com>
2022-08-04 16:37:48 +0100

    fecthComment model function added

--- models/articles.models.js
+++ models/articles.models.js
@@ -45,3 +45,29 @@ exports.updateVotes = (body, article_id) => {
       return response.rows[0];
     });
 };
+
+exports.fetchComment = article_id => {
+  return db
+    .query(
+      `
+  SELECT *
+  FROM articles
+  WHERE article_id=$1;
+  `,
+      [article_id]
+    )
+    .then(res => {
+      if (res.rowCount === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: "article_id not found",
+        });
+      } else {
+        return db
+          .query(`SELECT * FROM comments WHERE article_id = $1;`, [article_id])
+          .then(res => {
+            return res.rows;
+          });
+      }
+    });
+};
