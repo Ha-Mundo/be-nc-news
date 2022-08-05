ddea4eb711eae4ae775c9ffbd27aad3c5abdc7dd
Ha-Mundo <trauzzu@gmail.com>
2022-08-05 21:49:24 +0100

    fetchAllArticles model function updated

--- models/articles.models.js
+++ models/articles.models.js
@@ -1,13 +1,54 @@
 const db = require("../db/connection");
 
-exports.fetchAllArticles = () => {
+/* exports.fetchAllArticles = () => {
   return db
-    .query(
-      "SELECT articles.*, COUNT (comments.article_id) :: INTEGER AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id  GROUP BY articles.article_id ORDER BY created_at DESC"
+  .query(
+    "SELECT articles.*, COUNT (comments.article_id) :: INTEGER AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id  GROUP BY articles.article_id ORDER BY created_at DESC"
     )
     .then(({ rows }) => {
       return rows;
     });
+  }; */
+
+exports.fetchAllArticles = (sort_by = "created_at", order = "DESC", topic) => {
+  const allowedSortBY = [
+    "title",
+    "author",
+    "article_id",
+    "topic",
+    "votes",
+    "comment_count",
+    "created_at",
+  ];
+  const allowedTopics = ["mitch", "cats", "paper"];
+  const allowedOrder = ["ASC", "DESC"];
+
+  if (!allowedOrder.includes(order) || !allowedSortBY.includes(sort_by)) {
+    return Promise.reject({ status: 400, msg: "Bad Request" });
+  }
+
+  let queryStr = `SELECT articles.*, COUNT(comment_id) AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id`;
+
+  const topicValue = [];
+
+  if (topic) {
+    if (!allowedTopics.includes(topic)) {
+      return Promise.reject({ status: 404, msg: "Not found" });
+    } else {
+      queryStr += ` WHERE topic = $1`;
+      topicValue.push(topic);
+    }
+  }
+
+  queryStr += ` GROUP BY articles.article_id`;
+
+  const orderVariable = ` ORDER BY ${sort_by} ` + order + ";";
+
+  queryStr += orderVariable;
+
+  return db.query(queryStr, topicValue).then(reviews => {
+    return reviews.rows;
+  });
 };
 
 exports.selectArticleById = article_id => {
